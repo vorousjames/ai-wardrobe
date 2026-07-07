ec3702b614b6e9a77be722074e25a9577fc56111
iclawdius-maximus <iclawdius@heavenscapedevelopment.com>
2026-07-07 00:13:03 -0400

    Remove native module (placeholder) - fixes EAS pod install failure

--- modules/ai-renderer/src/index.ts
+++
@@ -1,5 +0,0 @@
-import AiRendererModule from './AiRendererModule';
-export { default as AiRendererModule } from './AiRendererModule';
-export * from './AiRenderer.types';
-
-export default AiRendererModule;
